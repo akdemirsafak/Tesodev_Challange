482d47047d4f9fe91c5a90d81f982d283776ffa7
akdemirsafak <akdemirsafak@gmail.com>
2023-11-26 01:02:47 +0300

    Basic gateway created

---
+++ Gateways/Tesodev.Gateway/Program.cs
@@ -0,0 +1,25 @@
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddAuthentication()
+//    .AddJwtBearer("GatewayAuthenticationScheme", opt => 
+//    {
+//        opt.Authority = builder.Configuration["IdentityServerURL"];
+//        opt.Audience = "resource_gateway";
+//        opt.RequireHttpsMetadata = false;
+//    });
+
+builder.Services.AddOcelot();
+
+builder.Configuration.AddJsonFile($"configuration.{builder.Environment.EnvironmentName}.json")
+    .AddEnvironmentVariables();
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+await app.UseOcelot();
+
+app.Run();
