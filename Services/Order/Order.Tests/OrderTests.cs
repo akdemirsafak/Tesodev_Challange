552823d6aabc6fdfbb70b94f89f9eef0913be697
akdemirsafak <akdemirsafak@gmail.com>
2023-11-26 18:14:51 +0300

    ProductTest completed.

---
+++ Services/Order/Order.Tests/OrderTests.cs
@@ -0,0 +1,17 @@
+﻿using NSubstitute;
+using Order.Core.Repositories;
+using Order.Core.UnifOfWorks;
+
+namespace Order.Tests;
+
+public class OrderTests
+{
+    private readonly IGenericRepository<Order.Core.Entities.Order> _orderRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    public OrderTests()
+    {
+        _orderRepository = Substitute.For<IGenericRepository<Order.Core.Entities.Order>>(); 
+        _unitOfWork = Substitute.For<IUnitOfWork>();
+    }
+
+}
